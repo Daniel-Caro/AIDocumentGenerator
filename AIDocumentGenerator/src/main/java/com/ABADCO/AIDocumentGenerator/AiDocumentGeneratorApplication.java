7c307f4e36009c43269545a96c7c9cd763c9c016
Daniel-Caro <danielcaroolmedo2@gmail.com>
2023-10-15 11:37:08 +0200

    Initial commit

---
+++ AIDocumentGenerator/src/main/java/com/ABADCO/AIDocumentGenerator/AiDocumentGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ABADCO.AIDocumentGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDocumentGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDocumentGeneratorApplication.class, args);
+	}
+
+}
---
+++ AIDocumentGenerator/src/test/java/com/ABADCO/AIDocumentGenerator/AiDocumentGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ABADCO.AIDocumentGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDocumentGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
